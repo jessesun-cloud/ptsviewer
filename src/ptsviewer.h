1001fe62486104fe04b55a60377b4d8e76c9c537
jian sun <75183234+jessesun-cloud@users.noreply.github.com>
2021-04-03 23:02:31 -0700

    cleanup.

--- src/ptsviewer.h
+++ src/ptsviewer.h
@@ -1,3 +1,4 @@
+#pragma once
 /*******************************************************************************
  *
  *       Filename:  ptsviewer.h
@@ -69,40 +70,8 @@ typedef struct
   boundingbox_t boundingbox;
 } cloud_t;
 
-
-
-/* Global variables */
-coord3d_t g_translate       =  { 0.0, 0.0, 0.0 };
-coord3d_t g_rot             =  { 0.0, 0.0, 0.0 };
-int       g_window          =                  0;
-int       g_mx              =                 -1;
-int       g_my              =                 -1;
-int       g_last_mousebtn   =                 -1;
-int       g_invertrotx      =                 -1;
-int       g_invertroty      =                 -1;
-float     g_zoom            =                  1;
-int       g_color           =                  1;
-float     g_pointsize       =               1.0f;
-cloud_t* g_clouds          =               NULL;
-uint32_t  g_cloudcount      =                  0;
-float     g_maxdim          =                  0;
-coord3d_t g_trans_center    =  { 0.0, 0.0, 0.0 };
-int       g_showcoord       =                  0;
-char      g_selection[1024] =                 "";
-float     g_movespeed       =                  1;
-int       g_left            =                -75;
-int       g_datastep        =                  0;
-
-boundingbox_t g_bb =
-{
-  { DBL_MAX, DBL_MAX, DBL_MAX },
-  { DBL_MIN, DBL_MIN, DBL_MIN }
-};
-
 /* Define viewer modes */
 
 #define VIEWER_MODE_NORMAL  0
 #define VIEWER_MODE_SELECT  1
 #define VIEWER_MODE_MOVESEL 2
-
-int       g_mode            = VIEWER_MODE_NORMAL;
